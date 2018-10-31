3d4e8d87a100ec0d7b05b796468e62f9fb74ccce
Aleksandr Chubutkin <chubutkin.a@applecity.kz>
2018-10-31 19:57:54 +0600

    Add code from React TODO app

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import ReactDOM from "react-dom";
 
-export default class App extends React.Component {
+import "./styles.css";
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
+let id = 0;
+
+const Todo = props => (
+  <li>
+    <input
+      type="checkbox"
+      checked={props.todo.checked}
+      onChange={props.onToggle}
+    />
+    <button onClick={props.onDelete}>delete</button>
+    <span>{props.todo.text}</span>
+  </li>
+);
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+
+  addTodo() {
+    let text = prompt("Please, write Todo: ");
+    if (text) {
+      this.setState({
+        todos: [...this.state.todos, { id: id++, text: text, checked: false }]
+      });
+    }
+  }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter(val => val.id !== id)
+    });
+  }
+
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.checked = !todo.checked;
+        }
+        return todo;
+      })
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <div>
+        <div>Todo count: {this.state.todos.length}</div>
+        <div>
+          Unchecked todos:{" "}
+          {this.state.todos.filter(todo => !todo.checked).length}
+        </div>
+        <button onClick={() => this.addTodo()}>Add TODO</button>
+        <ul>
+          {this.state.todos.map(todo => (
+            <Todo
+              onToggle={() => this.toggleTodo(todo.id)}
+              onDelete={() => this.removeTodo(todo.id)}
+              todo={todo}
+            />
+          ))}
+        </ul>
+      </div>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
